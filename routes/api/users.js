b5cd7c958aa3225d284697583623ae82027443b5
Blessing Sanusi <sanusi.temitope@ymail.com>
2020-01-02 17:48:52 -0600

    set up routers and schemas

---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   POST api/users
+// @desc    User registration    
+// @access Public
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('User route')
+});
+
+module.exports = router;
